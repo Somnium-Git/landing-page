d0c41aa1e28b6e39bbaba75d573267b9d550fee1
gabriellimao7500 <gabriellimao7500@gmail.com>
2025-04-22 14:41:30 -0300

    upd page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,6 @@
-'use client'
-import Button from '@/components/Button/Button'
-import Text from '@/components/Text/Text'
-
 export default function Home() {
-
-  const c = ()=>{
-    alert('oi')
-  }
-
   return (
     <div>
-      <Button>
-        
-      </Button>
-
     </div>
   )
 }
