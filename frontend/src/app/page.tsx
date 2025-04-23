f92365901b0e4982b3759ac196fe376a4cc912cd
gabriellimao7500 <gabriellimao7500@gmail.com>
2025-04-23 19:20:23 -0300

    udp

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,12 @@
 'use client'
 
-import Button from "@/components/Button/Button"
-import LightBall from "@/components/LightBall/LightBall"
-import Text from "@/components/Text/Text"
-
 import { useState } from "react"	
 
 export default function Home() {
   const [isActive, setIsActive] = useState(false)
   return (
     <div>
-      <Button text="Saiba mais" click={() => setIsActive(true)}>
-      </Button>
-      <LightBall x="40vw" y="-40vh" width="70vw" height="70vw" isActive={isActive}/>
-      <LightBall x="-7vw" y="75vh" width="60vw" height="60vw" isActive={isActive}/>
-      <LightBall x="-15vw" y="-60vh" width="50vw" height="50vw" isActive={isActive}/>
+
     </div>
   )
 }
