3dc61ed964df3030eb1c3ba674cd2bf54ab97805
thiago.dsm <thiagomeigmoreira18@gmail.com>
2025-04-29 19:14:10 -0300

    upd fonte da pagina + alterações footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
 // components/Footer.tsx
-import { FaFacebookF, FaInstagram, FaXTwitter } from 'react-icons/fa6';
+import { FaFacebook, FaXTwitter } from 'react-icons/fa6';
+import { AiFillInstagram } from "react-icons/ai";
+
+import Text from '../Text/Text';
 import Link from 'next/link';
 
 export default function Footer() {
@@ -8,32 +11,42 @@ export default function Footer() {
       {/* Topo do footer (logo, navegação e redes sociais) */}
       <div className="flex flex-col md:flex-row justify-between items-center md:items-start mb-10">
         <div className="mb-6 md:mb-0 text-center md:text-left">
-          <h2 className="text-white text-2xl font-bold">
-            Som<span className="text-purple-500">nium</span>
-          </h2>
-          <p className="text-gray-400 mt-1">Seu sucesso é o nosso sonho!</p>
+          <div className="cursor-pointer flex justify-center font-bold md:justify-start"><Text color="#FFFFFF" size="1.5rem">Som</Text><Text isGradient={true} size="1.5rem">nium</Text></div>
+          <Text color="#99a1af" size="1rem">Seu sucesso é o nosso sonho!</Text>
         </div>
 
-
-
-        <div className="flex gap-4 mt-6 md:mt-0">
-          <a href="#" className="text-purple-400 hover:text-white text-lg"><FaFacebookF /></a>
-          <a href="#" className="text-purple-400 hover:text-white text-lg"><FaInstagram /></a>
-          <a href="#" className="text-purple-400 hover:text-white text-lg"><FaXTwitter /></a>
+        <div className="flex items-center justify-center gap-4 mt-6 md:mt-0">
+          <a href="#" className="text-purple-400 hover:text-white text-2xl transition-all duration-200"><FaFacebook /></a>
+          <a href="#" className="text-purple-400 hover:text-white text-3xl transition-all duration-200"><AiFillInstagram /></a>
+          <a href="#" className="text-purple-400 hover:text-white text-2xl transition-all duration-200"><FaXTwitter /></a>
         </div>
       </div>
 
       <div className="flex flex-col md:flex-row justify-between items-center mb-6">
         <nav className="flex gap-6 flex-wrap justify-center md:justify-start">
-          <Link href="/" className="hover:text-white font-semibold">Home</Link>
-          <Link href="/sobre" className="hover:text-white">Sobre</Link>
-          <Link href="/projetos" className="hover:text-white">Projetos</Link>
-          <Link href="/parceiros" className="hover:text-white">Parceiros</Link>
-          <Link href="/contato" className="hover:text-white">Contato</Link>
+          <Link href="/" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">Home</Text>
+          </Link>
+          <Link href="/sobre" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">Sobre</Text>
+          </Link>
+          <Link href="/projetos" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">Projetos</Text>
+          </Link>
+          <Link href="/parceiros" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">Parceiros</Text>
+          </Link>
+          <Link href="/contato" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">Contato</Text>
+          </Link>
         </nav>
         <div className="flex gap-6 mb-4 md:mb-0">
-          <Link href="/termos" className="hover:text-white">TERMOS E CONDIÇÕES</Link>
-          <Link href="/privacidade" className="hover:text-white">POLÍTICA DE PRIVACIDADE</Link>
+          <Link href="/termos" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">TERMOS E CONDIÇÕES</Text>
+          </Link>
+          <Link href="/privacidade" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">POLÍTICA DE PRIVACIDADE</Text>            
+          </Link>
         </div>
       </div>
 
@@ -41,10 +54,9 @@ export default function Footer() {
       <hr className="border-t border-purple-900 mb-4" />
 
       {/* Parte inferior com navegação legal e copyright */}
-      <div className="flex flex-col md:flex-row justify-center items-center text-gray-400 text-xs">
-        <p className="text-center text-xs">
-          <span className="text-purple-500 font-semibold">Somnium</span> ©2025 - Todos os direitos reservados.
-        </p>
+      <div className="flex md:flex-row flex-cow justify-center items-center gap-1">
+        <Text color='oklch(71.4% 0.203 305.504)' size='1rem'>Somnium ©2025.</Text>
+        <Text>Todos os direitos reservados.</Text>
       </div>
     </footer>
   );
