147215e8294185565d3c2010e344ccc0e30a55c5
thiago.dsm <133548984+ethior0@users.noreply.github.com>
2025-05-26 21:27:30 -0300

    Dev (#1)
    * upd background
    
    * udp
    
    * header criado
    
    * upd header
    
    * Componente Text criado
    
    * upd header
    
    * upd header
    
    * udp
    
    * upd header e text
    
    * button +/- pronto
    
    * button pronto, adcionar estilos normais e hover, função de click e  filhos
    
    * upd css hover
    
    * button pronto
    
    * upd
    
    * upd packege.json
    
    * upd page.tsx
    
    * upd button
    
    * upd Button, Header, Text
    
    * upd global.css
    
    * card criado
    
    * lightball criada
    
    * udp
    
    * upd page.tsx
    
    * udp components
    
    * ligtball.tsx removida
    
    * Style, Footer finalizado
    
    * upd fonte da pagina + alterações footer
    
    * upd
    
    * upd gitidnore & package.json
    
    * upd merge
    
    ---------
    
    Co-authored-by: gabriellimao7500 <gabriellimao7500@gmail.com>
    Co-authored-by: Lucas Caribé Araújo <143850679+LucasCaribeAraujo@users.noreply.github.com>
    Co-authored-by: GMarsura <biel.marsura@gmail.com>
    Co-authored-by: EuAndersonDev <andersonaugustorei1227@gmail.com>

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,63 @@
+// components/Footer.tsx
+import { FaFacebook, FaXTwitter } from 'react-icons/fa6';
+import { AiFillInstagram } from "react-icons/ai";
+
+import Text from '../Text/Text';
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#09050f] text-gray-300 px-6 md:px-16 pt-10 pb-6 mt-20 text-sm"> 
+      {/* Topo do footer (logo, navegação e redes sociais) */}
+      <div className="flex flex-col md:flex-row justify-between items-center md:items-start mb-10">
+        <div className="mb-6 md:mb-0 text-center md:text-left">
+          <div className="cursor-pointer flex justify-center font-bold md:justify-start"><Text color="#FFFFFF" size="1.5rem">Som</Text><Text isGradient={true} size="1.5rem">nium</Text></div>
+          <Text color="#99a1af" size="1rem">Seu sucesso é o nosso sonho!</Text>
+        </div>
+
+        <div className="flex items-center justify-center gap-4 mt-6 md:mt-0">
+          <a href="#" className="text-purple-400 hover:text-white text-2xl transition-all duration-200"><FaFacebook /></a>
+          <a href="#" className="text-purple-400 hover:text-white text-3xl transition-all duration-200"><AiFillInstagram /></a>
+          <a href="#" className="text-purple-400 hover:text-white text-2xl transition-all duration-200"><FaXTwitter /></a>
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-between items-center mb-6">
+        <nav className="flex gap-6 flex-wrap justify-center md:justify-start">
+          <Link href="/" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">Home</Text>
+          </Link>
+          <Link href="/sobre" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">Sobre</Text>
+          </Link>
+          <Link href="/projetos" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">Projetos</Text>
+          </Link>
+          <Link href="/parceiros" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">Parceiros</Text>
+          </Link>
+          <Link href="/contato" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">Contato</Text>
+          </Link>
+        </nav>
+        <div className="flex gap-6 mb-4 md:mb-0">
+          <Link href="/termos" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">TERMOS E CONDIÇÕES</Text>
+          </Link>
+          <Link href="/privacidade" className="hover:text-white transition-all duration-200">
+            <Text size="1rem">POLÍTICA DE PRIVACIDADE</Text>            
+          </Link>
+        </div>
+      </div>
+
+      {/* Linha divisória roxa */}
+      <hr className="border-t border-purple-900 mb-4" />
+
+      {/* Parte inferior com navegação legal e copyright */}
+      <div className="flex md:flex-row flex-cow justify-center items-center gap-1">
+        <Text color='oklch(71.4% 0.203 305.504)' size='1rem'>Somnium ©2025.</Text>
+        <Text>Todos os direitos reservados.</Text>
+      </div>
+    </footer>
+  );
+}
