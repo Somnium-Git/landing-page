0239cdb5e65f29a99bb77e81060475a234b8de4c
thiago.dsm <thiagomeigmoreira18@gmail.com>
2025-06-01 21:37:34 -0300

    Revert "upd componente carrossel/slider"
    This reverts commit 030640546e3168f0cdf0017f2fed7295d9e1cfd1.

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,90 +1,88 @@
-"use client";
+'use client'
 
 import React, { useRef, useEffect, useState } from "react";
 import Text from "../Text/Text";
 
 type ButtonProps = {
-  styleCommon?: React.CSSProperties;
-  styleHover?: React.CSSProperties;
-  classCommon?: string;
-  classHover?: string;
-  click?: () => void;
-  text?: string;
-  image?: string;
-  children?: React.ReactNode;
+    styleCommon?: React.CSSProperties;
+    styleHover?: React.CSSProperties;
+    classCommon?: string;
+    classHover?: string;
+    click?: () => void;
+    text?: string;
+    image?: string;
+    children?: React.ReactNode;
 };
 
-const defaultStyleCommon: React.CSSProperties = {
-  width: "260px",
-  height: "50px",
-  backgroundColor: "#333333",
-  color: "#FFFFFF",
-  fontWeight: "bold",
-  letterSpacing: "1px",
-  borderRadius: "25px",
-  cursor: "pointer",
-};
+const defaultStyleCommon : React.CSSProperties = {
+    width: "280px",
+    height: "50px",
+    backgroundColor: "#333333",
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    letterSpacing: "1px",
+    borderRadius: "25px",
+    cursor: "pointer",
+}
 
-const defaultStyleHover: React.CSSProperties = {
-  ...defaultStyleCommon,
-  backgroundColor: "#111111",
-};
+const defaultStyleHover : React.CSSProperties = {
+    ...defaultStyleCommon,
+    backgroundColor: "#111111",
+}
 
 const defaultClassCommon = "button-common";
 const defaultClassHover = `${defaultClassCommon} button-hover`;
 
 function Button({
-  styleCommon = defaultStyleCommon,
-  styleHover = defaultStyleHover,
-  classCommon = defaultClassCommon,
-  classHover = defaultClassHover,
-  click = () => console.log("mouse clicked!"),
-  text = "",
-  image = "",
-  children,
+    styleCommon = defaultStyleCommon,
+    styleHover = defaultStyleHover,
+    classCommon = defaultClassCommon,
+    classHover = defaultClassHover,
+    click = () => console.log('mouse clicked!'),
+    text = "",
+    image = "",
+    children
 }: ButtonProps) {
-  const [styleCss, setStyleCss] = useState(styleCommon);
-  const [classCss, setClassCss] = useState(classCommon);
-  const [isGradient, setIsGradient] = useState(false);
-  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+    const [styleCss, setStyleCss] = useState(styleCommon);
+    const [classCss, setClassCss] = useState(classCommon);
+    const [isGradient, setIsGradient] = useState(false);
+    const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-  const handleHover = () => {
-    setStyleCss(styleHover);
-    setClassCss(classHover);
+    const handleHover = () => {
+        setStyleCss(styleHover);
+        setClassCss(classHover);
+        
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
 
-    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(() => {
+            setIsGradient(true);
+        }, 400);
+    };
+    
+    const handleUnhover = () => {
+        setStyleCss(styleCommon);
+        setClassCss(classCommon);
 
-    timeoutRef.current = setTimeout(() => {
-      setIsGradient(true);
-    }, 400);
-  };
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(() => {
+            setIsGradient(false);
+        }, 100);
+    };
 
-  const handleUnhover = () => {
-    setStyleCss(styleCommon);
-    setClassCss(classCommon);
-
-    if (timeoutRef.current) clearTimeout(timeoutRef.current);
-    timeoutRef.current = setTimeout(() => {
-      setIsGradient(false);
-    }, 100);
-  };
-
-  return (
-    <>
-      <button
-        style={styleCss}
-        className={classCss}
-        onMouseEnter={handleHover}
-        onMouseLeave={handleUnhover}
-        onClick={click}
-      >
-        <Text size="1.2em" isGradient={isGradient}>
-          {text}
-        </Text>
-        {children}
-      </button>
-    </>
-  );
+    return (
+        <>
+            <button 
+                style={styleCss}
+                className={classCss}
+                onMouseEnter={handleHover}
+                onMouseLeave={handleUnhover}
+                onClick={click}
+            >
+                <Text size="1.2em" isGradient={isGradient}>{text}</Text>
+                {children}
+            </button>
+        </>
+    )
 }
 
 export default Button;
--- frontend/src/components/Text/Text.tsx
+++ frontend/src/components/Text/Text.tsx
@@ -1,43 +1,30 @@
-// import PropTypes from 'prop-types';
-// import styles from "./Text.module.css"
+
+import PropTypes from 'prop-types';
+import styles from "./Text.module.css"
 
 type TextProps = {
-  children?: React.ReactNode;
-  color?: string;
-  size?: string;
-  isGradient?: boolean;
-  height?: string;
-  wordSpacing?: string;
-};
+    children?: React.ReactNode;
+    color?: string;
+    size?: string;
+    isGradient?: boolean; 
+    height?: string;
+    wordSpacing?: string;
+  };
 
-function Text({
-  children,
-  color,
-  size,
-  isGradient,
-  height = "light",
-  wordSpacing,
-}: TextProps) {
-  return (
-    <div
-      style={{
-        color: color,
-        fontSize: size,
-        fontWeight: height,
-        wordSpacing: wordSpacing,
-      }}
-      className={isGradient ? "text-gradient txt" : "txt"}
-    >
-      {children}
-    </div>
-  );
+function Text({ children, color, size, isGradient, height = "light", wordSpacing }: TextProps) {
+
+    return (
+        <div style={{color: color, fontSize: size, fontWeight: height, wordSpacing: wordSpacing}} className={isGradient ? "text-gradient txt" : "txt"}>
+            {children}
+        </div>
+    )
 }
 
 Text.defaultProps = {
-  Text: "",
-  color: "#fff",
-  size: "12px",
-  isGradient: false,
-};
+    Text: "",
+    color: "#fff",
+    size: "12px",
+    isGradient: false,
+}
 
 export default Text;
